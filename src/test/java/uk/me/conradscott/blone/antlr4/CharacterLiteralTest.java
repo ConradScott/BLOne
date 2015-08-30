d647099d5e0054e32f81adfa31c05e704759bb84
Conrad Scott <git@conradscott.me.uk>
2015-08-30 10:19:17 +0100

    Add some tests for character literal lexical analysis and parsing.

---
+++ src/test/java/uk/me/conradscott/blone/antlr4/CharacterLiteralTest.java
@@ -0,0 +1,43 @@
+package uk.me.conradscott.blone.antlr4;
+
+import org.antlr.v4.runtime.Token;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+@SuppressWarnings( { "HardcodedFileSeparator", "HardcodedLineSeparator" } ) public final class CharacterLiteralTest {
+    private static final String[] VALID_CHARACTER_LITERALS = { "\'a\'", "\'\\\\\'", "\'\\\"\'", "\'\\\\\'", "\'\\t\'" };
+
+    @Test public void testValidCharacters() {
+        for ( final String expected : VALID_CHARACTER_LITERALS ) {
+            final Token token = Lexer.getSingleTokenFromString( expected );
+            final String actual = token.getText();
+
+            assertEquals( "Character literal " + expected + " should be returned untouched by the lexical analyser",
+                          expected,
+                          actual );
+        }
+    }
+
+    private static final String[][] INVALID_CHARACTER_LITERALS = {
+        /* Empty character literal */ { "\'\'", "\'\'" },
+        /* Character literal too long */ { "\'aa\'", "\'" },
+        /* Unterminated escape */ { "\'\\\'", "\'\\\'" },
+        /* Unknown escape string */ { "\'\\x\'", "\'" },
+        /* Unterminated character literal */ { "\'", "\'" },
+        /* Unterminated character literal */ { "\'\\t", "\'\\t" },
+        /* Over-long octal escape */ { "\'\\423", "\'\\423" },
+        /* Over-long octal escape */ { "\'\\0123", "\'\\0123" },
+        /* Malformed octal escape */ { "\'\\8", "\'\\8" } };
+
+    @Test public void testInvalidCharacters() {
+        for ( final String[] entry : INVALID_CHARACTER_LITERALS ) {
+
+            final String actual = Lexer.getSingleInvalidTokenFromString( entry[ 0 ] );
+            final String expected = String.format( "token recognition error at: '%s'", entry[ 1 ] );
+
+            assertEquals( "Character literal " + entry[ 0 ] + " should have raised the syntax error \'" + entry[ 1 ] +
+                          "\'.", expected, actual );
+        }
+    }
+}
---
+++ src/test/java/uk/me/conradscott/blone/compiler/builder/CharacterBuilderTest.java
@@ -0,0 +1,73 @@
+package uk.me.conradscott.blone.compiler.builder;
+
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.TerminalNodeImpl;
+import org.junit.Test;
+import uk.me.conradscott.blone.antlr4.Lexer;
+import uk.me.conradscott.blone.ast.literal.CharacterLiteral;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+@SuppressWarnings( { "HardcodedFileSeparator", "HardcodedLineSeparator" } ) public final class CharacterBuilderTest {
+    @Test public void testLiteralsWithoutEscapes() {
+        final Character expected = 'a';
+        final Character actual = getCharacter( "\'a\'" );
+        assertEquals( "Character literals with no escapes should be parsed without change.", expected, actual );
+    }
+
+    @Test public void testEscapes() {
+        final String[] literals = { "\'\\b\'",
+                                    "\'\\t\'",
+                                    "\'\\n\'",
+                                    "\'\\f\'",
+                                    "\'\\r\'",
+                                    "\'\\\"\'",
+                                    "\'\\'\'",
+                                    "\'\\\\\'" };
+
+        final char[] expected = { '\b', '\t', '\n', '\f', '\r', '\"', '\'', '\\' };
+
+        assert expected.length == literals.length : "The two input arrays should be the same length.";
+
+        final char[] actual = new char[ expected.length ];
+
+        for ( int i = 0; i != expected.length; ++i ) {
+            actual[ i ] = getCharacter( literals[ i ] );
+        }
+
+        assertArrayEquals( "Escapes should be replaced with the corresponding character.", expected, actual );
+    }
+
+    @Test public void testOctalEscapes() {
+        final String[] literals = { "\'\\0\'",
+                                    "\'\\1\'",
+                                    "\'\\3\'",
+                                    "\'\\4\'",
+                                    "\'\\7\'",
+                                    "\'\\012\'",
+                                    "\'\\12\'",
+                                    "\'\\127\'",
+                                    "\'\\111\'",
+                                    "\'\\11\'" };
+
+        final char[] expected = { '\0', '\1', '\3', '\4', '\7', '\012', '\12', '\127', '\111', '\11' };
+
+        assert expected.length == literals.length : "The two input arrays should be the same length.";
+
+        final char[] actual = new char[ expected.length ];
+
+        for ( int i = 0; i != expected.length; ++i ) {
+            actual[ i ] = getCharacter( literals[ i ] );
+        }
+
+        assertArrayEquals( "Escapes should be replaced with the corresponding character.", expected, actual );
+    }
+
+    private static char getCharacter( final String s ) {
+        final Token token = Lexer.getSingleTokenFromString( s );
+        final CharacterLiteral literal = CharacterBuilder.build( new TerminalNodeImpl( token ) );
+
+        return literal.charValue();
+    }
+}
