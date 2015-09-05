9e52f7757a229405947bb363aa8798efb2b2de80
Conrad Scott <git@conradscott.me.uk>
2015-09-05 04:45:57 +0100

    Clean-up.

--- compiler/src/main/java/uk/me/conradscott/blone/compiler/printer/Formatter.java
+++ compiler/src/main/java/uk/me/conradscott/blone/compiler/printer/Formatter.java
@@ -15,7 +15,7 @@ final class Formatter {
     private Formatter() {}
 
     static void begin( final PrintStream out, final LocatedIfc located, final int depth ) {
-        out.format( "%n%s<%s: ", Formatter.indent( depth ), located.getClass().getSimpleName() );
+        out.format( "%n%s<%s: ", indent( depth ), located.getClass().getSimpleName() );
     }
 
     static void end( final PrintStream out ) {
@@ -24,7 +24,7 @@ final class Formatter {
 
     static void format( final PrintStream out, final String key, final String value, final int depth )
     {
-        out.format( "%n%s%s: %s", Formatter.indent( depth ), key, value );
+        out.format( "%n%s%s: %s", indent( depth ), key, value );
     }
 
     private static String indent( final int depth ) {
